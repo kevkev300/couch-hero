4a96d8e75f86c26a8777d86ccc831071ade3c1d9
Kevin Liebholz <kevin@liebholz.eu>
2020-04-04 09:51:18 +0200

    FEAT: created test and README for python

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
+    input_text = 1
+
+    # execute the python script, retrun has to be a string
+    @variable = `python lib/assets/python/test.py "#{input_text}"`
   end
 end
