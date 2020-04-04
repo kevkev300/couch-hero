dbddc79f4365f66d7f958f01caa3ca8fc18d130a
Kevin Liebholz <kevin@liebholz.eu>
2020-04-04 12:53:17 +0200

    FEAT: basic rails api

---
+++ app/controllers/api/v1/chatbot_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::ChatbotController < Api::V1::BaseController
+  def parse
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # resources :chatbot, only: [:create]
+      post '/chatbot', to: 'chatbot#parse'
+    end
+  end
 end
