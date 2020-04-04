262df74f88b4474d8f659209cd0278b4217564a2
Kevin Liebholz <kevin@liebholz.eu>
2020-04-04 23:27:47 +0200

    FIX: check if file is called correctly

--- lib/assets/python/request_help.py
+++ lib/assets/python/request_help.py
@@ -4,9 +4,11 @@ phone_no_r = sys.argv[1]
 zip_code = sys.argv[2]
 pastor = sys.argv[3]
 
-voluenteer = GET_VOLUENTEER(zip_code,pastor)
-phone_no_v = voluenteer[4]
+print(phone_no_r)
 
-SEND_SMS_VOLUENTEER(phone_no_v,phone_no_r)
+# voluenteer = GET_VOLUENTEER(zip_code,pastor)
+# phone_no_v = voluenteer[4]
 
-print("A person willing to help with shopping should contact soon using the following number: {}.".format(phone_no_v))
+# SEND_SMS_VOLUENTEER(phone_no_v,phone_no_r)
+
+# print("A person willing to help with shopping should contact soon using the following number: {}.".format(phone_no_v))
