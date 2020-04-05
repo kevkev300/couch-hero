9c07630129310411607df0d5b3fb18db6d550a47
ChristianHinge <christian@hingepedersen.dk>
2020-04-05 03:34:07 +0200

    Final fix

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -27,7 +27,6 @@ class Api::V1::ResponsesController < Api::V1::BaseController
     when 'request_help'
       str = stringify(parameters["phone-number"], parameters["zip-code"], parameters["last-name"])
       `python lib/assets/python/request_help.py "#{str}"`
-    end
     when 'voluenteer_help'
       str = stringify(parameters["phone-number"], parameters["zip-code"], parameters["last-name"])
       `python lib/assets/python/voluenteer_help.py "#{str}"`
